8c24c0b883ca489b90474bde804a867d3d6a7e29
Sam Beckmann <samvbeckmann@gmail.com>
2017-05-09 22:28:32 -0700

    Made default ability a trait.

--- js/editor-ui.js
+++ js/editor-ui.js
@@ -207,7 +207,7 @@ function makeTraitAbilityLine(name, desc) {
   var content = `
     <div class="stacked-text-blocks">
       <textarea class="ability-trait-name ability-field form-control common expandable no-dup-top no-dup-right no-dup-left" rows="1" placeholder="Name">${name}</textarea>
-      <textarea class="ability-trait-desc ability-field form-control common expandable no-dup-borders"" rows="1" placeholder="Description">${desc}</textarea>
+      <textarea class="ability-trait-desc ability-field form-control common expandable no-dup-borders" rows="1" placeholder="Description">${desc}</textarea>
     </div>
   `;
   return makeGenericAbility('Trait', 'abilityline-trait', content);
