b9d7115e3af4565de01bd46c05adb015fbdff389
Sam Beckmann <sam@samvbeckmann.com>
2019-01-01 23:17:47 -0600

    Added autosizing to all textareas

--- src/components/EditHeader.vue
+++ src/components/EditHeader.vue
@@ -2,27 +2,27 @@
       <div class="form-group mb-0">
         <div class="input-group">
           <span class="input-group-addon no-rounded-corners no-dup-top col-md-3 col-2">Name</span>
-          <input
+          <textarea
             class="form-control no-rounded-corners no-dup-top"
-            type="text"
             id="monster-name"
             v-model="active_monster.name"
-          >
+            rows="1"
+          ></textarea>
         </div>
         <div class="input-group">
           <span class="input-group-addon no-rounded-corners no-dup-top col-md-3 col-2">Header</span>
-          <input
+          <textarea
             class="form-control no-rounded-corners no-dup-top"
-            type="text"
             id="monster-header"
             v-model="active_monster.heading"
-          >
+            rows="1"
+          ></textarea>
         </div>
       </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters } from 'vuex';
 
 export default {
   computed: {
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store.js'
+import autosize from 'autosize'
 
 new Vue({
   el: 'app',
   store,
   components:{App}
 })
+
+autosize(document.querySelectorAll('textarea'))
