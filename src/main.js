8700a2a5839053d88a53397ce53bca36b39c0ceb
Sam Beckmann <sam@samvbeckmann.com>
2019-01-17 22:10:23 -0800

    Removed unneeded code from main.js

--- src/main.js
+++ src/main.js
@@ -10,26 +10,3 @@ new Vue({
   store,
   components:{App}
 })
-
-Vue.directive('autosize', {
-	bind: function() {
-		var self = this
-			Vue.nextTick(function() {
-			autosize(self.el)
-		})
-	},
-
-	update: function(value) {
-		var self = this
-		Vue.nextTick(function() {
-			self.el.value = value
-			autosize.update(self.el)
-		})
-	},
-
-	unbind: function() {
-		autosize.destroy(this.el)
-	}
-});
-
-//autosize(document.querySelectorAll('textarea'))
